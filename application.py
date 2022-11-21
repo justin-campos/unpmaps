ddfb93847c1bd9ab5548cde878477af96fbbfdb3
KodyStudio <justinsilvac360@gmail.com>
2022-11-21 12:36:34 -0600

    session del usuario! :D

--- application.py
+++ application.py
@@ -17,11 +17,22 @@ db = SQL("sqlite:///unpmaps.db")
 
 app.config['SECRET_KEY'] = 'reemplazar_clave_secreta'
 
+# Ensure responses aren't cached
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 # Instancia de SocketIo
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 # Configure session to use filesystem
-app.config["SESSION_FILE_DIR"] = False
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
@@ -245,7 +256,3 @@ def saludar(mensaje, user, fecha):
 
     # Enviar respuesta de evento emit al cliente
     return (f'{mensaje}')
-
-
-if __name__ == '__main__':
-    app.debug = True
